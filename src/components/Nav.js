87bcf12f33c375772f6a02f53108e72eed092c1a
osayi bishop <eguaborbishop1@gmail.com>
2020-02-18 11:05:31 +0100

    consumed colorModeContext context in nav class

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,23 +1,27 @@
-import React from 'react'
 import { NavLink } from 'react-router-dom'
+import React, { Component } from 'react'
+import ColorModeContext from "../context/colorMode"
 
-const Nav = () => {
-  return (
-    <nav className='nav'>
-      <ul>
-        <li>
-          <NavLink to='/' exact activeClassName='active'>
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/new' activeClassName='active'>
-            New Tweet
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
-  )
-} 
-
-export default Nav;
+export default class Nav extends Component {
+  static contextType = ColorModeContext
+    render() {
+      let  color = this.context.colorMode;
+      let style = {"color":color.color}; 
+      return (
+        <nav className='nav'>
+        <ul>
+          <li>
+            <NavLink style={style} to='/' exact activeClassName='active'>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink style={style} to='/new' activeClassName='active'>
+              New Tweet
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+      )
+  }
+}
