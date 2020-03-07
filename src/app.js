657120004ea4fa9133dfc6fe39f3cb73118faf43
osayi bishop <eguaborbishop1@gmail.com>
2020-03-07 09:56:11 +0100

    created result page

--- src/app.js
+++ src/app.js
@@ -1,24 +1,10 @@
 import React, { Component } from 'react'
-import "./index.css"
-import ResultHeader from './component/ResultHeader'
-import Card from './component/Card'
 
 export default class app extends Component {
     render() {
         return (
             <div>
-                <ResultHeader />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
+                hello
             </div>
         )
     }
---
+++ src/component/ResultPage.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import "./index.css"
+import ResultHeader from './component/ResultHeader'
+import Card from './component/Card'
+
+export default class ResultPage extends Component {
+    render() {
+        return (
+            <div>
+                <ResultHeader />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+            </div>
+        )
+    }
+}
