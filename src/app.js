2941f284569f5e81e81a9df562b598eca9dfbb1a
osayi bishop <eguaborbishop1@gmail.com>
2020-03-07 10:36:37 +0100

    added voice search mic icon

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import { FaHamburger } from 'react-icons/fa'
-import {MdMenu} from 'react-icons/all'
-    // <TiThMenu color={"red"}/>
+
 import SearchPage from './component/SeachPage'
 export default class app extends Component {
     render() {
--- src/component/SearchInput.js
+++ src/component/SearchInput.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { MdMic } from 'react-icons/all'
+
 
 function SearchInput() {
     return <div className="search-container">
-        <input type="text" />
-        <div className="search-mic">M</div>
-        <div className="search-button">S</div>
-    </div>;
+                <input type="text" />
+                <div className="search-mic">
+                    <MdMic 
+                        color={"gray"} 
+                        size={"2rem"} />
+                </div>
+                <div className="search-button">S</div>
+            </div>;
 }
 
 export default SearchInput;
+
