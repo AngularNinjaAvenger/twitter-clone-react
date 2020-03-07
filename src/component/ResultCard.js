fec89e07a22fb908b640ebb1ee28032d242725a9
osayi bishop <eguaborbishop1@gmail.com>
2020-03-07 14:15:53 +0100

    save guarding routes

--- src/component/ResultCard.js
+++ src/component/ResultCard.js
@@ -1,17 +1,18 @@
 import React from 'react';
 
-function ResultCard({url,title,description,date}) {
+function ResultCard({url,title,description,date,swapterm}) {
     url = url ? url : "http://www.google.com/lattest-news"
     title = title ? title : "lattest news form google for front end engineers"
     description = description ? description : "this is the latttest new from google front end engineers are poeple who build the front end of applications,they design,build, test , they make sure the requirement for the application is met and they deploy"
     date = date ? date : "20 Oct 2019"
+    swapterm = swapterm ? swapterm:"swapterm"
     return (
         <div className="card-container">
-            <span className="result-link">{url}</span>
-            <a className="result-title" href="">{title}</a>
+            <span className="result-link">{url.replace("searchwod",swapterm)}</span>
+            <a className="result-title" href="">{title.replace("searchwod",swapterm)}</a>
             <div className="description-container">
-                <span className="date">{date}</span> :
-                <span className="description">{description}</span>
+                <span className="date">{date.replace("searchwod",swapterm)}</span> :
+                <span className="description">{description.replace("searchwod",swapterm)}</span>
             </div>
         </div>
     )
--- src/component/ResultPage.js
+++ src/component/ResultPage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import ResultHeader from './ResultHeader'
 import ResultCard from './ResultCard'
 import result from "../MockData.json"
+import { Redirect } from 'react-router-dom'
 export default class ResultPage extends Component {
     constructor(props) {
         super(props)
@@ -11,14 +12,20 @@ export default class ResultPage extends Component {
         }
     }
     componentDidMount() {
-        console.log(result);
+
+        console.log();
         this.setState({ result });
     }
     render() {
+        try{
+            const term = this.props.location.query.term;
+        }catch(e){
+            return <Redirect to="/" />;
+        }
         return (
             <div>
                 <ResultHeader />
-                {this.state.result.map((item,idx)=><ResultCard key={idx } data={result} />)}
+                {this.state.result.map((item,idx)=><ResultCard swapterm={term} key={idx} data={result} />)}
             </div>
         )
     }
