10a9ff12c8ed575f78bd098a7125346c8f14f64a
osayi bishop <eguaborbishop1@gmail.com>
2020-03-07 11:27:40 +0100

    consumed mock json data

--- src/component/ResultCard.js
+++ src/component/ResultCard.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
-function ResultCard() {
+function ResultCard({url,title,description,date}) {
+    url = url ? url : "http://www.google.com/lattest-news"
+    title = title ? title : "lattest news form google for front end engineers"
+    description = description ? description : "this is the latttest new from google front end engineers are poeple who build the front end of applications,they design,build, test , they make sure the requirement for the application is met and they deploy"
+    date = date ? date : "20 Oct 2019"
     return (
         <div className="card-container">
-            <span className="result-link">http:\\www.google.com/lattest-news</span>
-            <a className="result-title" href="">this is the page title</a>
+            <span className="result-link">{url}</span>
+            <a className="result-title" href="">{title}</a>
             <div className="description-container">
-                <span className="date">20 Oct 2019</span> :
-                <span className="description">
-                    this is the lattest new that is coming into the new tetiplac this is 
-                    the lattest new that is coming into the new tetiplac this is the lattest 
-                    new that is coming into the new tetiplac 
-                </span>
+                <span className="date">{date}</span> :
+                <span className="description">{description}</span>
             </div>
         </div>
     )
--- src/component/ResultPage.js
+++ src/component/ResultPage.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react'
 import ResultHeader from './ResultHeader'
 import ResultCard from './ResultCard'
-
+import result from "../MockData.json"
 export default class ResultPage extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             result:[]
+        }
+    }
+    componentDidMount() {
+        console.log(result);
+        this.setState({ result });
+    }
     render() {
         return (
             <div>
                 <ResultHeader />
-                <ResultCard />
-                <ResultCard />
-                <ResultCard />
-                <ResultCard />
+                {this.state.result.map((item,idx)=><ResultCard key={idx } data={result} />)}
             </div>
         )
     }
