2f807a5c10a9b8c0b39ba8cd46da8c2884b0cedc
osayi bishop <eguaborbishop1@gmail.com>
2020-03-07 07:56:58 +0100

    created search button and search page

--- src/app.js
+++ src/app.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react'
 import Nav from './component/nav/Nav'
-import "index.css"
+import "./index.css"
 export default class app extends Component {
     render() {
         return (
             <div>
                 <Nav />
-                <div className="google-icon-wrapper">
-                    <img src={process.env.PUBLIC_URL + "/google-home-logo.png"} alt="google-icon" />
-                </div>
-                <div>
-                    <input type="text" />
-                    <div>M</div>
-                    <div>S</div>
+
+                <div className="container">
+                    <div className="google-icon-wrapper">
+                        <img src={process.env.PUBLIC_URL + "/google-home-logo.png"} alt="google-icon" />
+                    </div>
+
+                    <div className="search-container">
+                        <input type="text" />
+                        <div className="search-mic">M</div>
+                        <div className="search-button">S</div>
+                    </div>
                 </div>
             </div>
         )
