3cdc6c11326c7ffd8a4e5966b0b5bb23408e6a6c
osayi bishop <eguaborbishop1@gmail.com>
2020-03-07 13:06:47 +0100

    fixed error it's route not router

--- src/app.js
+++ src/app.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react'
 import { FaHamburger } from 'react-icons/fa'
 
 import SearchPage from './component/SeachPage'
+import { BrowserRouter, Route } from 'react-router-dom'
+import ResultPage from './component/ResultPage'
 export default class app extends Component {
     render() {
         return (
-            <div>
-                <SearchPage />
-            </div>
+            <BrowserRouter>
+                <Route path='/' exact component={SearchPage} />
+                <Route path='/result' exact component={ResultPage} />
+            </BrowserRouter>
         )
     }
 }
