791d437e1684f60db245cbcedc17fefb1f95a711
osayi bishop <eguaborbishop1@gmail.com>
2020-03-07 06:47:46 +0100

    created nav bar

--- src/component/nav/Nav.js
+++ src/component/nav/Nav.js
@@ -1,16 +1,23 @@
 import React from 'react'
+import "./nav.css"
 
 export default function Nav() {
     return (
         <header>
-            <nav class="left-nav">
-                HAM
+            <nav className="left-nav">
+                H
                 <ul>
                     <li><a href="">all</a></li>
                     <li><a href="">home</a></li>
                 </ul>
             </nav>
-            <nav>side 2</nav>
+
+            <nav className="right-nav">
+                <div>O</div>
+                <div>O</div>
+                <div>O</div>
+            </nav>
+
         </header>
     )
 }
